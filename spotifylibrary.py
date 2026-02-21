384162fb2eadcefad4bc1f776502608e5c10cae5
cruces13 <cruces13@gmail.com>
2026-02-21 11:08:59 -0500

    Added spotifylibrary.py and main.py, adding to working branch. main.py is the entry point for the program, and spotifylibrary.py contains functions for interacting with the Spotify API. The main function in main.py will create playlists in YT Music based on the playlists in Spotify, and add the songs to the YT Music playlists.

---
+++ spotifylibrary.py
@@ -0,0 +1,29 @@
+
+
+
+class SpotifyLibrary:
+    def __init__(self):
+        # Initialize the Spotify library
+        playlists = []
+        likedSongs = []
+        likedAlbums = []
+
+    def get_playlists(self):
+        # Get the playlists from Spotify by sending request to API
+        pass
+
+    def get_songs_from_playlist(self, playlist):
+        # Get the songs from the Spotify playlist by sending request to API
+        pass
+
+    def get_liked_songs(self):
+        # Get the liked songs from Spotify by sending request to API
+        pass
+
+    def get_liked_albums(self):
+        # Get the liked albums from Spotify by sending request to API
+        pass
+
+    def get_liked_artists(self):
+        # Get the liked artists from Spotify by sending request to API
+        pass
